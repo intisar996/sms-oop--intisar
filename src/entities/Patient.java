bf576b792064e5c900c0d4af13689f9f486f837a
intisar <intisar19961@hotmail.com>
2026-08-19 11:57:59 +0400

    Patient extends Person + fields   [Task 1.2]

---
+++ src/entities/Patient.java
@@ -0,0 +1,49 @@
+package entities;
+
+import java.util.Date;
+import java.util.List;
+
+public class Patient extends  Person{
+
+
+
+    private String bloodGroup;
+    private String emergencyContact;
+    private Date registrationDate;
+    private List<String> allergies;
+    private List<String>  pastMedicalRecordIds;
+    private Double outstandingBalance;
+    private Boolean isInsured;
+
+
+    public Patient(boolean active_status, String address, Integer age, Date dateOfBirth, String email, String firstName, String gender, long id, String lastName, String nationalId, String phoneNumber, List<String> allergies, String bloodGroup, String emergencyContact, Boolean isInsured, Double outstandingBalance, List<String> pastMedicalRecordIds, Date registrationDate) {
+        super(active_status, address, age, dateOfBirth, email, firstName, gender, id, lastName, nationalId, phoneNumber);
+        this.allergies = allergies;
+        this.bloodGroup = bloodGroup;
+        this.emergencyContact = emergencyContact;
+        this.isInsured = isInsured;
+        this.outstandingBalance = outstandingBalance;
+        this.pastMedicalRecordIds = pastMedicalRecordIds;
+        this.registrationDate = registrationDate;
+    }
+
+    public Patient(long id, String firstName, String lastName, List<String> allergies, String bloodGroup, String emergencyContact, Boolean isInsured, Double outstandingBalance, List<String> pastMedicalRecordIds, Date registrationDate) {
+        super(id, firstName, lastName);
+        this.allergies = allergies;
+        this.bloodGroup = bloodGroup;
+        this.emergencyContact = emergencyContact;
+        this.isInsured = isInsured;
+        this.outstandingBalance = outstandingBalance;
+        this.pastMedicalRecordIds = pastMedicalRecordIds;
+        this.registrationDate = registrationDate;
+    }
+
+
+
+
+
+
+
+
+
+}
